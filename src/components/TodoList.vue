882bfd2267290f63b4e2f07bf935a2ba6529eed0
MEK <ansenfrl@gmail.com>
2020-12-27 06:22:56 +0900

    TodoList 컴포넌트 UI 스타일링

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <ul>
-      <li v-for='todoItem in todoItems' v-bind:key='todoItem'>
+      <li v-for='todoItem in todoItems' v-bind:key='todoItem' class="shadow">
         {{todoItem}}
+      <span class="removeBtn" v-on:click='removeTodo' >
+            <font-awesome-icon icon="trash-alt"/>
+      </span>
       </li>
     </ul>
   </div>
@@ -10,28 +13,63 @@
 
 <script>
 export default {
-  
-  data(){
+  data() {
     return {
-      todoItems:[]
+      todoItems: [],
+    };
+  },
+  methods: {
+    removeTodo() {},
+  },
+  //생성되는시점에 실행되는 라이프사이클
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        this.todoItems.push(key);
+      }
     }
   },
-  
-  //생성되는시점에 실행되는 라이프사이클
-  created(){
-    if(localStorage.length>0){
-      for(let i=0; i<localStorage.length; i++) {
-        let key = localStorage.key(i);
-        this.todoItems.push(key)
-}
-
-
-}
-  }
-
-}
+};
 </script>
 
 <style scope>
+ul{
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li{
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.checkBtn{
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted{
+  color: #b3adad;
+}
+
+.textCompleted{
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+.removeBtn{
+  margin-left: auto;
+  color: #de4343;
+}
 
 </style>
