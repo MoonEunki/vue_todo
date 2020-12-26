a32bb9489e797213519a62968ba65d48d6aca06d
MEK <ansenfrl@gmail.com>
2020-12-27 05:52:20 +0900

    fontawesome 관련 설치 및 Todoinput 개발

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,60 @@
 <template>
-    <div>인풋</div>
+<div class="inputBox shadow">
+  <!--  v-on:keyup.enter='addTodo' 는 input에서 엔터쳤을때 실행 -->
+  <input v-model="newTodoItem" placeholder="edit me" v-on:keyup.enter='addTodo' >
+
+  <span class="addContainer">
+    <font-awesome-icon icon="plus" v-on:click='addTodo'/>
+  </span>
+
+</div>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      newTodoItem:''
+    }
+  },
+  methods:{
+    addTodo(){
+      //로컬 스토리지에 k/v 로 저장하기
+      localStorage.setItem(this.newTodoItem,this.newTodoItem)
+      this.clearInput()
+      },
+
+    clearInput(){
+      this.newTodoItem=''
+    }
+  }
 
 }
 </script>
 
-<style>
-
+<style scope>
+input:focus{
+  outline: none;
+}
+.inputBox{
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input{
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer{
+  float: right;
+  background: linear-gradient(to right,#6478FB,#8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn{
+  color: white;
+  vertical-align: 0 5px 5px 0;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-Vue.config.productionTip = false
+// library.add(faUserSecret);
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faPlus);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   components: { App },
+//   template: '<App/>'
+// })
