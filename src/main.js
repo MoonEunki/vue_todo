2093c4a1f5dd27f3164a9c78dfda1d0ce950b4d8
MEK <ansenfrl@gmail.com>
2020-12-27 09:29:34 +0900

    Vuex 설치및 등록

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App';
 import './fontAwesomeIcon.js'; // fontAwesomeIcon.js 불러옴
+// import { store } from './store/store.js';
 
 Vue.config.productionTip = false;
 
 new Vue({
+  // store,
   render: (h) => h(App),
 }).$mount('#app');
